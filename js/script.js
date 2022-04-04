5592b988eca6758e8f5e417bc0682d339e597564
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-04-04 18:40:20 +0200

    conclusa creazione griglie campo minato

--- js/script.js
+++ js/script.js
@@ -1,23 +1,70 @@
 
-// GRID EASY
-const colNumber = 100;
+function generaCampoMinato() {
 
- function  stampareGriglia(){
-     let grid = document.getElementById('grid');
-     let colsCreate = creaColonne(); 
-     console.log(colsCreate);
-     grid.innerHTML = colsCreate;
- }
+    const colNumberFacile = 100;
+    const colNumberMedio = 81;
+    const colNumberDifficile = 49;
 
- function creaColonne(){
-     let cols  = '';
-     for(let i = 1; i <= colNumber; i++){
-      cols += `
+    let livello = document.getElementById("sceltaLivello").value;
+    console.log(livello);
+
+    if (livello === 'facilissimo') {
+        function stampareGrigliaFacile() {
+            let grid = document.getElementById('grid');
+            let colsCreate = creaColonneFacile();
+            console.log(colsCreate);
+            grid.innerHTML = colsCreate;
+        }
+
+        function creaColonneFacile() {
+            let cols = '';
+            for (let i = 1; i <= colNumberFacile; i++) {
+                cols += `
       <div class="grid-facile bordo"> ${i}</div>
       `;
-     }
-     return cols;
- }
+            }
+            return cols;
+        }
+        stampareGrigliaFacile();
 
- // GENERIAMO HTML CON BOTTONE
-  document.getElementById('bottone').addEventListener('click', stampareGriglia);
+    } else if (livello === 'medissimo') {
+        function stampareGrigliaMedio() {
+            let grid = document.getElementById('grid');
+            let colsCreate = creaColonneMedio();
+            console.log(colsCreate);
+            grid.innerHTML = colsCreate;
+        }
+
+        function creaColonneMedio() {
+            let cols = '';
+            for (let i = 1; i <= colNumberMedio; i++) {
+                cols += `
+         <div class="grid-medio bordo"> ${i}</div>
+         `;
+            }
+            return cols;
+        }
+        stampareGrigliaMedio();
+    } else if (livello = 'difficilissimo') {
+        function stampareGrigliaDifficile() {
+            let grid = document.getElementById('grid');
+            let colsCreate = creaColonneDifficile();
+            console.log(colsCreate);
+            grid.innerHTML = colsCreate;
+        }
+
+        function creaColonneDifficile() {
+            let cols = '';
+            for (let i = 1; i <= colNumberDifficile; i++) {
+                cols += `
+         <div class="grid-difficile bordo"> ${i}</div>
+         `;
+            }
+            return cols;
+        }
+        stampareGrigliaDifficile();
+    }
+}
+
+document.getElementById('bottone').addEventListener('click', generaCampoMinato);
+
